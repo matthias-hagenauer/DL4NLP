2c4a874b68d4b533d909ebcb0954806c30ce4696
Ruben Figge <rfigge@hotmail.com>
2025-09-29 15:48:52 +0200

    test with pipe

--- DifficultyEstimation/estimate_difficulty.py
+++ DifficultyEstimation/estimate_difficulty.py
@@ -2,12 +2,14 @@ import sentinel_metric
 from sentinel_metric import download_model, load_from_checkpoint
  
 # Load model directly
-from transformers import AutoModel
-model = AutoModel.from_pretrained("Prosho/sentinel-src-24", torch_dtype="auto")
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("translation", model="Prosho/sentinel-src-24")
 
 data = [
     {"src": "Please sign the form."},
     {"src": "He spilled the beans, then backpedaled—talk about mixed signals!"}
 ]
 
-output = model.predict(data, batch_size=8, gpus=1)
+output = pipe(data)
